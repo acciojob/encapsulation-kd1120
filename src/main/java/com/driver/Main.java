536945bde62402f8a9e9aa30d6825574639167f4
khushboo <khushidudeja45@gmail.com>
2023-06-10 16:22:09 +0530

    first try

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
-  
+
+    RWOnly obj = new RWOnly();
+
+    //obj.name = "Kashish";
+
+    public void setObj(RWOnly obj) {
+        this.obj = obj;
+    }
+    public RWOnly getObj() {
+        return obj;
+    }
+
+
 }
